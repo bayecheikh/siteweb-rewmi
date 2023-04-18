d2d8d882bee6b13afd8835b06cb73fbed9e1f60d
Cheikh Gueye <bayecheikhgueye2@gmail.com>
2023-04-18 12:35:31 +0000

    first commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
 
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: 'GEC CITOYEN',
+        title: 'REWMI',
         titleTemplate: '%s',
         htmlAttrs: {
             lang: 'zxx'
